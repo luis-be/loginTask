04011de4a74e5719c6bd1260324c4e89d222ba10
Casiano <luis.benitez.ayala@gmail.com>
2024-11-26 21:21:34 -0300

    Update
    update del login, requerimientos y modelo de base de datos

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.core.mail import send_mail
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    nombre_de_usuario = models.CharField(
+        _('Nombre de Usuario'),
+        max_length=150,
+    )
+    correo_electronico = models.EmailField(
+        _('Correo Electrónico'),
+        unique=True,
+    )
+    edad = models.PositiveIntegerField(_('Edad'), default=0, blank=True)  # edad
+    es_superusuario = models.BooleanField(default=False)
+    es_personal = models.BooleanField(_('Es Personal'), default=False)
+    esta_activo = models.BooleanField(_('Está activo'), default=True)
+    fecha_union = models.DateTimeField(_('Fecha que se unió'), default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'correo_electronico'
+    USERNAME_FIELD = 'correo_electronico'
+    REQUIRED_FIELDS = ['nombre_de_usuario']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        db_table = 'users'
+        swappable = 'AUTH_USER_MODEL'
+
+    def clean(self):
+        super().clean()
+        self.correo_electronico = self.__class__.objects.normalize_email(self.correo_electronico)
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        send_mail(subject, message, from_email, [self.correo_electronico], **kwargs)
--- accounts/views.py
+++ accounts/views.py
@@ -2,18 +2,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login, authenticate
+from .forms import CustomUserCreationForm
 
 class UserCreateAndLoginView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     template_name = "accounts/signup.html"
     success_url = reverse_lazy("tasks:index")
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        username = form.cleaned_data.get("username")
+        correo_electronico = form.cleaned_data.get("correo_electronico")
         raw_pw = form.cleaned_data.get("password1")
-        user = authenticate(username=username, password=raw_pw)
-        if user is not None:
-            login(self.request, user)
+        user = authenticate(correo_electronico=correo_electronico, password=raw_pw)
+        login(self.request, user)
         return response
- 
